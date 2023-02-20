cdf07cd39977100b3dc9e6f6832c98c28602723b
agaa-mac <agaa.mac@gmail.com>
2023-02-20 22:40:10 +0100

    forecast is working

--- src/ForecastWeather.js
+++ src/ForecastWeather.js
@@ -16,66 +16,16 @@ export default function ForecastWeather(props) {
     return (
       <div className="col-5">
         <div className="Forecast">
-          <WeatherForecastDay data={forecast[0]} />
           <div className="row ForecastInfo">
-            <div className="col ForecastDay">Tuesday</div>
-            <div className="col Icon">
-              <ReactAnimatedWeather
-                icon="CLEAR_DAY"
-                color="black"
-                size="30"
-                animate={true}
-              />
-            </div>
-            <div className="col Temperature">7 °C</div>
-          </div>
-          <div className="row ForecastInfo">
-            <div className="col ForecastDay">Wednesday</div>
-            <div className="col">
-              <ReactAnimatedWeather
-                icon="CLEAR_DAY"
-                color="black"
-                size="30"
-                animate={true}
-              />
-            </div>
-            <div className="col Temperature">7 °C</div>
-          </div>
-          <div className="row ForecastInfo">
-            <div className="col ForecastDay">Thursday</div>
-            <div className="col">
-              <ReactAnimatedWeather
-                icon="CLEAR_DAY"
-                color="black"
-                size="30"
-                animate={true}
-              />
-            </div>
-            <div className="col Temperature">7 °C</div>
-          </div>
-          <div className="row ForecastInfo">
-            <div className="col ForecastDay">Friday</div>
-            <div className="col">
-              <ReactAnimatedWeather
-                icon="CLEAR_DAY"
-                color="black"
-                size="30"
-                animate={true}
-              />
-            </div>
-            <div className="col Temperature">7 °C</div>
-          </div>
-          <div className="row ForecastInfo">
-            <div className="col ForecastDay">Saturday</div>
-            <div className="col">
-              <ReactAnimatedWeather
-                icon="CLEAR_DAY"
-                color="black"
-                size="30"
-                animate={true}
-              />
-            </div>
-            <div className="col Temperature">7 °C</div>
+            {forecast.map(function (dailyForecast, index) {
+              if (index < 6) {
+                return (
+                  <div className="col" key={index}>
+                    <WeatherForecastDay data={dailyForecast} />
+                  </div>
+                );
+              }
+            })}
           </div>
         </div>
       </div>
